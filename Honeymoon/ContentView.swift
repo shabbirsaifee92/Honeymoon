11d2b846ba98fa2609c1fc5c92f71d722eb9d18b
shabbirsaifee92 <shabbirsaifee91@gmail.com>
2020-07-24 16:11:10 -0400

    Initial Commit

---
+++ Honeymoon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Honeymoon
+//
+//  Created by Shabbir Saifee on 7/24/20.
+//  Copyright © 2020 Shabbir Saifee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
