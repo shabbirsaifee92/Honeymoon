fcd42e5b1b0d8fac81c2f92bc56e1de32df4067a
shabbirsaifee92 <shabbirsaifee91@gmail.com>
2020-07-25 00:51:42 -0400

    add guide view

---
+++ Honeymoon/View/GuideView.swift
@@ -0,0 +1,63 @@
+//
+//  GuideView.swift
+//  Honeymoon
+//
+//  Created by Shabbir Saifee on 7/25/20.
+//  Copyright © 2020 Shabbir Saifee. All rights reserved.
+//
+
+import SwiftUI
+
+struct GuideView: View {
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                HeaderComponent()
+                
+                Spacer(minLength: 10)
+                
+                Text("Get Started")
+                    .fontWeight(.black)
+                    .font(.largeTitle)
+                    .foregroundColor(.pink)
+                
+                Text("Discover and pick the perfect destination for your romantic honeymoon!")
+                    .lineLimit(nil)
+                    .multilineTextAlignment(.center)
+                
+                Spacer(minLength: 10)
+                
+                VStack(alignment: .leading, spacing: 25) {
+                    GuideComponent(title: "Like", subtitle: "Swipe Right", description: "Do you like this destination? Touch the screen and swipe right, It will save to the favorites.", icon: "heart.circle")
+                    
+                    GuideComponent(title: "Dismiss", subtitle: "Swipe Left", description: "Would you rather skip this place? Touch the screen and swipe left. You will no longer see the it.", icon: "xmark.circle")
+                    
+                    GuideComponent(title: "Book", subtitle: "Tap the button", description: "Our selection for the honeymoon resorts is perfect setting for you to embark your new life together.", icon: "checkmark.square")
+                }
+                
+                Spacer(minLength: 10)
+                
+                Button(action: {
+                    print("Button tapped")
+                }) {
+                    Text("Continue".uppercased())
+                        .font(.headline)
+                        .padding()
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .background(Capsule().fill(Color.pink))
+                        .foregroundColor(.white)
+                }
+            }
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding(.top, 15)
+            .padding(.bottom, 25)
+            .padding(.horizontal, 25)
+        }
+    }
+}
+
+struct GuideView_Previews: PreviewProvider {
+    static var previews: some View {
+        GuideView()
+    }
+}
