1c6904fd1c860f447c402ca282c4dcc40af8f4b2
shabbirsaifee92 <shabbirsaifee91@gmail.com>
2020-07-25 00:18:40 -0400

    add footer view

--- Honeymoon/View/ContentView.swift
+++ Honeymoon/View/ContentView.swift
@@ -19,6 +19,7 @@ struct ContentView: View {
             CardView(honeymoon: honeymoonData[2])
                 .padding()
             Spacer()
+            FooterView()
         }
     }
 }
---
+++ Honeymoon/View/FooterView.swift
@@ -0,0 +1,46 @@
+//
+//  FooterView.swift
+//  Honeymoon
+//
+//  Created by Shabbir Saifee on 7/25/20.
+//  Copyright © 2020 Shabbir Saifee. All rights reserved.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "xmark.circle")
+                .font(.system(size: 42, weight: .light))
+            Spacer()
+            
+            Button(action: {
+                print("Success")
+            }) {
+                Text("Book Destination".uppercased())
+                    .font(.system(.subheadline, design: .rounded))
+                    .fontWeight(.heavy)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 12)
+                    .accentColor(.pink)
+                    .background(
+                        Capsule().stroke(Color.pink, lineWidth: 2)
+                )
+            }
+            
+            Spacer()
+            
+            Image(systemName: "heart.circle")
+                .font(.system(size: 42, weight: .light))
+        }
+        .padding()
+    }
+}
+
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+            .previewLayout(.fixed(width: 375, height: 80))
+    }
+}
